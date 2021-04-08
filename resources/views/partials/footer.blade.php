225a7759cc208e7856ecf537e3173793b36e7b5a
fransisco <fransiscolova@gmail.com>
2021-04-08 12:05:07 +0000

    update

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,10 @@
+<div class="fullwidth-sidebar-container">
+    <div class="sidebar bottom-sidebar"></div>
+</div>
+<footer id="footer" class="site-footer" role="contentinfo">
+    <div class="copyright">
+        <div class="container">
+            <p class="copyright-text"> Copyright &copy; 2020 <a href='Kebun Digital' target='_blank' title='Directory Starter'> Kebun Digital</a>.</p>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,25 @@
+<header id="site-header" class="site-header" role="banner">
+    <div class="container">
+        <div class="site-logo-wrap">
+            <hgroup>
+                <h1 style ="align:center" class='site-title site-title-no-desc'> <a  style="color:#FFFFFF;" href='{{ route('home') }}' title='CariTempat' rel='home'>CariTempat</a></h1>
+            </hgroup>
+        </div>
+        <nav id="primary-nav" class="primary-nav" role="navigation">
+            <ul id="menu-gd-menu" class="menu">
+                @guest
+                    <li><a href="#">Directory tempat wisata </a></li>
+                     <!--
+                    <li class="gd-menu-item menu-item menu-item-type-post_type_archive"><a href="{{ route('register') }}">Register</a></li> -->
+                @else
+                    <li class="gd-menu-item menu-item menu-item-type-post_type_archive"><a href="{{ route('admin.shops.index') }}">Kelola Tempat</a></li>
+                    <li class="gd-menu-item menu-item menu-item-type-post_type_archive"><a href="#" onclick="event.preventDefault(); document.getElementById('logoutform').submit();">Logout</a></li>
+                    <form id="logoutform" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
+                @endguest
+            </ul>
+        </nav>
+        <div class="dt-nav-toggle  dt-mobile-nav-button-wrap"><a href="#primary-nav"><i class="fas fa-bars"></i></a></div>
+    </div>
+</header>
