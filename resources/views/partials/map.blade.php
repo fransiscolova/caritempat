e70b855aabf856e556e138beef69f97f1ac9fc65
fransiscos <frans@pohondana.id>
2021-04-09 18:58:56 +0700

    no message

--- resources/views/partials/map.blade.php
+++ resources/views/partials/map.blade.php
@@ -1,6 +1,6 @@
 <div class="fullwidth-sidebar-container">
     <div class="sidebar top-sidebar">
-        <div id="map-canvas" style="height: 250px; width: 100%; position: relative; overflow: hidden;">
+        <div id="map-canvas" style="height: 300px; width: 100%; position: relative; overflow: hidden;">
         </div>
     </div>
 </div>
