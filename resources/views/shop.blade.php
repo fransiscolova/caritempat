db1b69da4ac45d87876f26d82134b18f3c152442
fransiscos <frans@pohondana.id>
2021-04-09 18:53:34 +0700

    no message

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -172,7 +172,7 @@ $(function(){
         function initialize() {
             var latLng = new google.maps.LatLng({{ $shop->latitude }}, {{ $shop->longitude }});
             var mapOptions = {
-                zoom: 6,
+                zoom: 10,
                 minZoom: 6,
                 maxZoom: 17,
                 zoomControl:true,
