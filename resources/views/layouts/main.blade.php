107cef1d9f9ea1ed35cd418471836fe31e1549af
fransiscos <frans@pohondana.id>
2021-04-10 07:40:01 +0700

    add analytic

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width">
     <title>{{ config('app.name', 'Cari Tempat') }}</title>
-	
+	<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.3.3/firebase-app.js"></script>
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/8.3.3/firebase-analytics.js"></script>
     <link rel='stylesheet' href='{{ asset('assets/css/style.css') }}' type='text/css' />
     <link rel='stylesheet' href='{{ asset('assets/css/jquery.mmenu.css') }}' type='text/css' />
     <link rel='stylesheet' href='{{ asset('assets/css/responsive.css') }}' type='text/css' />
@@ -14,6 +18,26 @@
     <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.11.2/css/v4-shims.css?wpfas=true' type='text/css' />
     @yield('styles')
 </head>
+
+
+
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyAh6yq22oRboEPMd54GcE-GYXudE0LIgPU",
+    authDomain: "cari-tempat-a7350.firebaseapp.com",
+    projectId: "cari-tempat-a7350",
+    storageBucket: "cari-tempat-a7350.appspot.com",
+    messagingSenderId: "623879707337",
+    appId: "1:623879707337:web:6d7d015e2b8cfba20f8b18",
+    measurementId: "G-G1CB01ESEY"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
+
 
 <body class="archive post-type-archive post-type-archive-gd_place geodir_custom_posts geodir-page geodir-archive geodir_advance_search gd-map-auto">
     <div id="ds-container">
